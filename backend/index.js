687fc2e6118619876b5906ed0f83a3732fb8f16d
Aravind-a12 <102940905+Aravind-a12@users.noreply.github.com>
2022-09-18 18:58:34 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const mysql = require('mysql')
+const cors = require ('cors')
+
+const db = mysql.createConnection({
+    connectionLimit: '10',
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodedb'
+});
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+
+app.post("/insert", (req,res) => {
+const firstname = req.body.firstname
+const secondname = req.body.secondname
+
+    const sqlInsert = "INSERT INTO my_name (firstname, secondname) VALUES (?,?)";
+    db.query(sqlInsert, [firstname, secondname], (err, result) => {
+        console.log(err);
+    });
+});
+
+app.listen(3001, () =>{
+    console.log("Server running in PORT 3001");
+})
